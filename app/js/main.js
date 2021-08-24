610a5ae4b2fb932b4a166c57a6af907f78900ac7
unknown <pogrebnyak19999@gmail.com>
2021-08-24 15:20:07 +0300

    Верстка десктоп версии страницы о нас

--- app/js/main.js
+++ app/js/main.js
@@ -109,7 +109,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     $(window).scroll(function(){
       $('.title').each(function(index, item) {
-        console.log()
+        console.log('1')
         var wt = $(window).scrollTop();
         var wh = $(window).height();
         var et = $(item).offset().top;
@@ -120,4 +120,46 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }
       })
     });
+
+    $(document).ready(function () {
+
+        var show = true;
+        var countbox = ".benefits__inner";
+        $(window).on("scroll load resize", function () {
+            if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+            var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+            var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+            var w_height = $(window).height(); // Высота окна браузера
+            var d_height = $(document).height(); // Высота всего документа
+            var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+            if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+                $('.benefits__number').css('opacity', '1');
+                $('.benefits__number').spincrement({
+                    thousandSeparator: "",
+                    duration: 1200
+                });
+                
+                show = false;
+            }
+        });
+
+    });
+
+    function openDocument() {
+      document.querySelectorAll('.useful__item').forEach(item => {
+          item.addEventListener('click', function() {
+              document.querySelector('.modal__block img').src = this.querySelector('img').src
+              document.querySelector('.modal').classList.add('modal__show')
+          })
+          document.querySelector('.modal').addEventListener('click', (item) => {
+              if (item.target.classList.contains('modal__block')) {
+                  document.querySelector('.modal').classList.remove('modal__show')
+              }
+          })
+          document.querySelector('.modal__close').addEventListener('click', (item) => {
+              document.querySelector('.modal').classList.remove('modal__show')
+          })
+      })
+  }
+  openDocument();
 });
